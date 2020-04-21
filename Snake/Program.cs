f29df4f0b07bc65a0c4c8748cd3d20220a8a27d6
CancerSia <jefferysia7511@gmail.com>
2020-04-22 00:57:56 +0800

    Revert "2 features"
    This reverts commit bb26313ca314757758a8a3f3545be1af9e635f93.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -40,7 +40,7 @@ namespace Snake
             byte down = 2;
             byte up = 3;
             int lastFoodTime = 0;
-            int foodDissapearTime = 16000;
+            int foodDissapearTime = 8000;
             int negativePoints = 0;
             string userName;
             Console.WriteLine("Enter your name: ");
@@ -62,16 +62,11 @@ namespace Snake
 			//intialise the position of first 5 obstacle
             List<Position> obstacles = new List<Position>()
             {
-                new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)),
-                new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)),
+                new Position(12, 12),
+                new Position(14, 20),
+                new Position(7, 7),
+                new Position(19, 19),
+                new Position(6, 9),
             };
 
 			//produce obstacles item on certain position with Cyan coloured "="
